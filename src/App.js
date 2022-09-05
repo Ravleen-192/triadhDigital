5d3873ba4e62a3dd44ede54c3261cab01b1c3441
Ravleen-192 <meravleen@gmail.com>
2022-09-05 16:45:07 +0530

    Product Menu with Helios etc render issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
+
 import { node } from 'prop-types';
 import './css/App.css';
 import './css/bootstrap.min.css';
+import ImportComponent from './common/ImportComponent';
+import Home from "./component/Home";
+import Services from "./component/Services";
+import Products from "./component/Products";
+import Company from "./component/Company";
+import Capabilities from "./component/Capabilities";
+
 import Theia from "./component/Theia";
 import Helios from "./component/Helios";
 import Plutus from "./component/Plutus";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-
+import { positions, Provider } from "react-alert";
+import AlertMUITemplate from "react-alert-template-mui";
 import {
   Collapse,
   Navbar,
@@ -19,45 +26,44 @@ import {
   NavItem,
   NavLink, Col
 } from 'reactstrap';
-import ImportComponent from './common/ImportComponent';
+
 import logo from './image/logo_new.png';
-import { positions, Provider } from "react-alert";
-import AlertMUITemplate from "react-alert-template-mui";
 
-// import instagram from "./image/instagram.JPEG";
-// import linkedin from './image/linkin.png';
-// import twitter from './image/twitter.png'
-const links = [
-  { href: '#Home', text: '', menuId: 0 },
-  { href: '#services', text: 'Services', menuId: 1, },
-  { href: '#products', text: 'Products', menuId: 2, },
-  { href: '#company', text: 'Company', menuId: 3, },
-  { href: '#capabilities', text: 'Capabilities', menuId: 4, },
-  { className: "frmbtn2", href: '#contact', text: 'Lets Talk', menuId: 5, },
-
-];
-
-const createNavItem = ({ href, text, className, menuId }) => (
-  <NavItem key={menuId}>
-    <NavLink href={href} className={className} onClick={() => mContext.onClickMenuItem(menuId)}>{text}</NavLink>
-  </NavItem>
-
-);
 var mContext;
 const options = {
   timeout: 10000,
   position: positions.MIDDLE
 };
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
       isOpen: false,
       child: node,
+      user: null, // will contain our user data object when signed in
+      status: "Home", loaded: true, hasError: false, type: "", message: "",
+      noti_open: false, user_open: false,
+      links: [
+        { href: '#Home', text: '', menuId: 0, source: "Home", isActive: true },
+        { href: '#Services', text: 'Services', menuId: 1, source: "Services", isActive: false },
+        { href: '#Products', text: 'Products', menuId: 2, source: "Products", isActive: false },
+        { href: '#Company', text: 'Company', menuId: 3, source: "Company", isActive: false },
+        { href: '#Capabilities', text: 'Capabilities', menuId: 4, source: "Capabilities", isActive: false },
+        { className: "frmbtn2", href: '#Contact', text: 'Lets Talk', menuId: 5, source: "contact", isActive: false },
+
+      ],
+      ProdLinks: [
+        { href: '#Helios', text: 'Helios', menuId: 0, source: "Helios", isActive: true },
+        { href: "#Theia", text: "Theia", menuId: 1, source: "Theia", isActive: false },
+        { href: '#Plutus', text: 'Plutus', menuId: 2, source: "Plutus", isActive: false },
+      ]
     };
     mContext = this;
+    this.toggle = this.toggle.bind(this);
+    this.setOnLoad = this.setOnLoad.bind(this);
     this.onClickMenuItem = this.onClickMenuItem.bind(this);
     this.toggle = this.toggle.bind(this);
+    
   }
   componentWillMount() {
     this.onClickMenuItem(0);
@@ -69,21 +75,13 @@ export default class App extends Component {
     this.setState({ child: <Component.default onClickMenuItem={this.onClickMenuItem}></Component.default> });
     this.closeNavbar()
   }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  closeNavbar() {
-    if (this.state.isOpen === true) {
-      this.toggle();
-    }
-  }
   newTab(linkedin) {
     var url = linkedin ? "https://www.linkedin.com/company/triadh-ai/?viewAsMember=true" : " https://www.instagram.com/triadhai/";
     window.open(url, "_blank");
   }
+  
   componentDidMount() {
+    this.setOnLoad(false);
     window.history.pushState(null, null, window.location.pathname);
     window.addEventListener('popstate', this.onBackButtonEvent);
   }
@@ -95,16 +93,16 @@ export default class App extends Component {
         case "Home":
           this.onClickMenuItem(0);
           break;
-        case "company":
+        case "Company":
           this.onClickMenuItem(1);
           break;
-        case "services":
+        case "Services":
           this.onClickMenuItem(2);
           break
-        case "products":
+        case "Products":
           this.onClickMenuItem(3);
           break;
-        case "capabilities":
+        case "Capabilities":
           this.onClickMenuItem(4);
           break;
         case "contact":
@@ -122,73 +120,286 @@ export default class App extends Component {
   componentWillUnmount = () => {
     window.removeEventListener('popstate', this.onBackButtonEvent);
   }
+  
+  AuthComponent = () => {
+    switch (this.state.status) {
+      case "Home":
+        return( <Home
+          switchComponent={this.switchComponent}
+          setOnLoad={this.setOnLoad}
+          
+        />
+      );
+      case "Services":
+        return (
+          <Services
+            switchComponent={this.switchComponent}
+            setOnLoad={this.setOnLoad}            
+          />
+        );
+      case "Company":
+        return (
+          <Company
+            switchComponent={this.switchComponent}
+            
+            setOnLoad={this.setOnLoad}
+           
+          />
+        );
+
+      case "Capabilities":
+        return (
+          <Capabilities
+            switchComponent={this.switchComponent}
+           
+            setOnLoad={this.setOnLoad}
+           
+          />
+        );
+        case "Products":
+          
+          return (
+            <Products
+              switchComponent={this.switchComponent}
+             
+              setOnLoad={this.setOnLoad}
+              
+            />
+          );
+          case "Helios":
+          
+          return (
+            <Helios
+              switchComponent={this.switchComponent}
+             
+              setOnLoad={this.setOnLoad}
+              
+            />
+          );
+          case "Theia":
+          
+            return (
+              <Theia
+                switchComponent={this.switchComponent}
+               
+                setOnLoad={this.setOnLoad}
+                
+              />
+            );
+            case "Plutus":
+          
+              return (
+                <Plutus
+                  switchComponent={this.switchComponent}
+                 
+                  setOnLoad={this.setOnLoad}
+                  
+                />
+              );
+      default:
+        break;
+    }
+  };
+  switchComponent = status => {
+    let { ProdLinks} = this.state;
+    this.setState({ status });
+    if (status === "Products") {
+      mContext.setState({ user: status });
+      this.setState({ ProdLinks });
+
+    }
+    console.log("status = ",status);
+    this.closeNavbar();
+  };
+  
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  closeNavbar() {
+    if (this.state.isOpen == true) {
+      this.toggle();
+    }
+  }
+  setOnLoad(source) {
+    this.setState({ loaded: source });
+  }
+  setUser(user) {
+    mContext.setState({ user: user });
+  }
+  onHidden(hidden) {
+    mContext.setState({ hasError: hidden.hasError });
+  }
+  
+  scroll(container, key, loop = false) {
+    var element = document.getElementById(container);
+    if (loop && this.state.mouseout) {
+      return;
+    }
+    if (element) {
+      var scroll_height = element.scrollTop;
+      switch (key) {
+        case "top":
+          if (scroll_height > 0) {
+            scroll_height -= 10;
+            element.scrollTop = scroll_height;
+            window.setTimeout(() => { this.scroll(container, key, true) }, 100);
+          }
+          break;
+        case "bottom":
+          var scrollable_height = scroll_height + element.clientHeight;
+          if (scrollable_height < element.scrollHeight) {
+            scroll_height += 10;
+            element.scrollTop = scroll_height;
+            window.setTimeout(() => { this.scroll(container, key, true) }, 100);
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+  toggleNavMenu(nav_item) {
+    const { noti_open, user_open } = this.state;
+    switch (nav_item) {
+      case "noti":
+        this.setState({ theam_open: false, fav_open: false, noti_open: !noti_open, other_noti_open: false, user_open: false });
+        break;
+      case "user":
+        this.setState({ theam_open: false, fav_open: false, noti_open: false, other_noti_open: false, user_open: !user_open });
+        break;
+      default:
+        this.setState({ noti_open: false, user_open: false });
+        break;
+    }
+  }
+  setMenuActive(source, key) {
+    let { links, ProdLinks} = this.state;
+    switch (source) {
+      case "Landing":
+        links.map((element, index) => {
+          element.isActive = index == key ? true : false;
+        });
+        this.setState({ links });
+        break;
+        
+      case "Helios":
+        ProdLinks.map((element, index) => {
+          element.isActive = index == key ? true : false;
+        });
+        this.setState({ ProdLinks });
+        break;
+      default:
+        break;
+    }
+  }
   render() {
-    let { child } = this.state;
+    let { noti_open,user,  ProdLinks, links, child} = this.state;
     var year = new Date().getFullYear();
     return (
-      <div>
-        <BrowserRouter>
-
-          <Navbar light expand="md" sticky={'top'} className="navbar-header">
+      <section>
+        <div>
+        {/*<Router>*/}
+          {!user ? <Navbar light expand="md" sticky={'top'} className="navbar-header">
 
             <NavbarBrand href="/"><img className="img img-responsive navbrand" src={logo} alt="logo" /></NavbarBrand>
+
             {this.state.isOpen ?
-              <a onClick={this.toggle} type="button" className="navbar-toggle pull-right closebtn"><i className="fa fa-times" aria-hidden="true"></i></a>
+              <a onClick={this.toggle} type="button" className="navbar-toggle pull-right closebtn">X</a>
               :
               <NavbarToggler onClick={this.toggle} />
             }
 
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
-                {links.map(createNavItem)}
+                {links.map((element, key) => {
+                  return (
+                    <NavItem key={element.menuId} active={element.isActive}>
+                      <NavLink active={element.isActive} href={element.href} className={element.className}
+                        onClick={() => { mContext.switchComponent(element.source); this.setMenuActive("Landing", key) }}>{element.text}</NavLink>
+                    </NavItem>
+                  )
+                })}
               </Nav>
             </Collapse>
+          </Navbar> :
+            <Navbar light expand="md" sticky={'top'} className="navbar-header">
 
-          </Navbar>
+              <NavbarBrand href="/"><img className="img img-responsive navbrand" src={logo} alt="logo" /></NavbarBrand>
 
-          <Provider template={AlertMUITemplate} {...options}>
-            <div>{child}</div>
-          </Provider>
+              {this.state.isOpen ?
+                <a onClick={this.toggle} type="button" className="navbar-toggle pull-right closebtn">X</a>
+                :
+                <NavbarToggler onClick={this.toggle} />
+              }
 
-          <footer className="footer">
-            <Col className="row">
-              <Col className="" md="12" sm="12" xs="12" lg="12" xl="12">
-                <Col md="12" sm="12" xs="12" lg="12" xl="12" className="text-center">
-                  <Col className="footer-text">
-                    <ul className="footer-ul">
-                      <li className="list-inline-item">
-                        <a className="social-icon link" onClick={() => this.newTab(true)}>
-                          <i className="fa fa-linkedin-square"></i>
-                        </a>
-                      </li>
-                      {/* <li className="list-inline-item">
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto custom-menu" navbar>
+                  {ProdLinks.map((element, key) => {
+                    return (
+                      <NavItem key={element.menuId} active={element.isActive}>
+                        <NavLink active={element.isActive} href={element.href} className={element.className}
+                          onClick={() => { mContext.switchComponent(element.source); this.setMenuActive("Helios", key) }}>{element.text}</NavLink>
+                      </NavItem>
+                    )
+                  })}
+                  <NavItem className={noti_open ? "dropdown notifications-menu open" : "dropdown notifications-menu"} >
+                    <a onClick={() => { this.toggleNavMenu("noti") }} />
+                  </NavItem>
+                  <NavItem 
+                    onClick={() => {
+                      mContext.switchComponent("Products");
+                      ProdLinks.map((element, index) => {
+                        element.isActive = false;
+                      });
+                      this.setState({ ProdLinks });
+                    }}>
+                    
+                    
+                  </NavItem>
+                  
+                </Nav>
+              </Collapse>
+            </Navbar>}
+          <div>{this.AuthComponent()}</div>
+        <footer className="footer">
+          <Col className="row">
+            <Col className="" md="12" sm="12" xs="12" lg="12" xl="12">
+              <Col md="12" sm="12" xs="12" lg="12" xl="12" className="text-center">
+                <Col className="footer-text">
+                  <ul className="footer-ul">
+                    <li className="list-inline-item">
+                      <a className="social-icon link" onClick={() => this.newTab(true)}>
+                        <i className="fa fa-linkedin-square"></i>
+                      </a>
+                    </li>
+                    {/* <li className="list-inline-item">
                   <a className="social-icon" target="_blank" href="#">
                     <i className="fa fa-twitter"></i>
                   </a>
                 </li> */}
-                      <li className="list-inline-item">
-                        <a className="social-icon link" onClick={() => this.newTab(false)}>
-                          <i className="fa fa-instagram"></i>
-                        </a>
-                      </li>
-                      <li className="list-inline-item">
-                        <p>© {year} Triadh</p>
-                      </li>
-                    </ul>
-                  </Col>
+                    <li className="list-inline-item">
+                      <a className="social-icon link" onClick={() => this.newTab(false)}>
+                        <i className="fa fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li className="list-inline-item">
+                      <p>© {year} Triadh</p>
+                    </li>
+                  </ul>
                 </Col>
               </Col>
             </Col>
-          </footer>
-          <Switch>
-            <Route path="/Theia" component={Theia} />
-            <Route path="/Helios" component={Helios} />
-            <Route path="/Plutus" component={Plutus} />
-          </Switch>
-        </BrowserRouter>
-
-      </div >
-
+          </Col>
+        </footer>
+       
+        </div>
+        
+      </section>
     );
   }
 }
+
+export default App;
--- src/component/Products.js
+++ src/component/Products.js
@@ -3,7 +3,8 @@ import { Link, BrowserRouter as Router } from "react-router-dom";
 
 import icon1 from '../image/icon1.png';
 import icon2 from '../image/icon2.png';
-import  icon3 from '../image/icon3.png';
+import icon3 from '../image/icon3.png';
+import Helios from "./Helios";
 
 export default class Products extends Component {
   constructor(props) {
@@ -25,43 +26,8 @@ export default class Products extends Component {
 
   render() {
     return (
-      
-      <div className="td__header section__padding" id="home">
-        <div className="td__Prodheader-content prod-text">
-        <Link to="/Helios">
-          <img
-            src={icon1}
-            alt="Helios"
-          />       
-          <h3 >Helios</h3>
-          <p>Machine vision platform</p>
-          </Link>
-          </div>
-          <div className="td__Prodheader-content  prod-text">
-          <Link to="/Theia">
-          <img
-            src={icon2}
-            alt="Theia"
-          />
-       
-            <h3 >Theia</h3>
-            <p>Data mesh portal </p>
-            </Link>
-            
-          </div>
-          <div className="td__Prodheader-content  prod-text">
-          <Link to="/Plutus">
-          <img
-            src={icon3}
-            alt="Plutus"
-          />
-        
-            <h3 >Plutus </h3>
-            <p>Data product catalog</p>
-            </Link>
-          </div>
-        </div>
-        
-        );
+    <Helios />
+
+    );
   }
 }
